6a6b56b77fe0bb534463c2a3ab93a65694e05986
opilane <opilane@HP-CZC2349HT3>
2023-05-10 10:21:45 +0300

    Add project files.

---
+++ andmebaas/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using andmebaas.Models;
+using andmebaas.Views;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace andmebaas
+{
+    public partial class App : Application
+    {
+        public const string DATABASE_NAME = "friends.db";
+        public static FriendRepository database;
+        public static FriendRepository Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new FriendRepository(
+                        Path.Combine(
+                            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new DBListPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
